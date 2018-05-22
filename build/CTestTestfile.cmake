c50a6353d31424bedaa697d48aec0154d6d3a22d
Takuya Iwai <takuya.iwai.p@tier4.jp>
2018-05-22 20:22:01 +0900

    mv .git

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tier4-t-iwai/program/colmap_sfm/src
+# Build directory: /home/tier4-t-iwai/program/colmap_sfm/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(colmap)
---
+++ build/colmap/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tier4-t-iwai/program/colmap_sfm/src/colmap
+# Build directory: /home/tier4-t-iwai/program/colmap_sfm/build/colmap
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
