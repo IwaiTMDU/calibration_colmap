f77e36d96d555b2bd26c0a555b0cba998ead6334
Takuya Iwai <takuya.iwai.p@tier4.jp>
2018-07-26 20:28:15 +0900

    create launch file

--- build/colmap/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tier4-t-iwai/program/colmap_sfm/src/colmap
-# Build directory: /home/tier4-t-iwai/program/colmap_sfm/build/colmap
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/colmap/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "colmap"
-PROJECT_SPACE_DIR = "/home/tier4-t-iwai/program/colmap_sfm/devel"
-PROJECT_VERSION = "0.0.0"
